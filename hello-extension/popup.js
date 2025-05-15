bcde24567d875a89bfe2369345578c67a45d2f32
Beno-Jeffry-B <benojeffry08@gmail.com>
2025-05-15 23:09:07 +0530

    popup.js is added with alert()

---
+++ hello-extension/popup.js
@@ -0,0 +1,6 @@
+document.getElementById("click_me").addEventListener( "click", () =>{
+    alert("Boom Boom Boommmer.....!")
+} )
+
+// addEventListener has two paramerters...!
+// "yetha panna?" .addEventListener("enna event" , " enna pannanum")
